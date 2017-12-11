a0ae87ab5137c07e56902e18360614e23aa3004b
Filip Stangricki <stangricki@gmail.com>
2017-12-11 18:52:51 +0100

    Added jQuery to script.js file

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ function getJoke() {
 }
 
 getJoke()
+
+/* Same code as above, however with usage of jQuery
+
+var url = 'http://api.icndb.com/jokes/random';
+var $button = $('#get-joke').click(function(){getJoke()});
+var $paragraph = $('#joke');
+
+function getJoke() {
+	$.ajax({
+		method: 'GET',
+		url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+		success: function(res) {
+			$paragraph.text(res.value.joke);
+		}
+	});
+}
+
+getJoke()
+
+*/
